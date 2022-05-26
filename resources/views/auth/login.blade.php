5ed371f58955403c168b6f3d4fc9d9c5f71704d5
dianakrupko <dnkrupkoo@gmail.com>
2022-05-26 22:04:02 +0300

    new

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,74 @@
+@extends('layouts.app')
+<link href="/css/bootstrap-grid.min.css" rel="stylesheet">
+
+<link rel="stylesheet" href="/css/general.css">
+<link rel="stylesheet" href="/css/login.css">
+@section('content')
+<div class="container">
+    <div class="box-form">
+                <h2>{{ __('Вхід в кабінет') }}</h2>
+
+                <div class="box">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+
+                        <div class="param">
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Логін') }}</label>
+
+                            <div class="">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="param">
+                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Пароль') }}</label>
+
+                            <div class="">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="">
+                            <div class="">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label class="form-check-label" for="remember">
+                                        {{ __("Запам'ятати мене") }}
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="">
+                            <div class="">
+
+                                @if (Route::has('password.request'))
+                                    <a class="forgot-password" href="{{ route('password.request') }}">
+                                        {{ __('Забули свій пароль?') }}
+                                    </a>
+                                @endif
+                                <div class="button">
+                                    <button type="submit" class="button-login">
+                                        {{ __('Увійти') }}
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.app')
+<link href="/css/bootstrap-grid.min.css" rel="stylesheet">
+
+<link rel="stylesheet" href="/css/general.css">
+<link rel="stylesheet" href="/css/register.css">
+@section('content')
+<div class="container">
+    <div class="box-form">
+                <h2>{{ __('Реєстрація') }}</h2>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
+
+                        <div class="param">
+                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __("Ім'я") }}</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="param">
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="param">
+                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Пароль') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="param">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Повторіть пароль') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            </div>
+                        </div>
+
+                        <div class="button">
+                                <button type="submit" class="button-register">
+                                    {{ __('Зареєструватися') }}
+                                </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
